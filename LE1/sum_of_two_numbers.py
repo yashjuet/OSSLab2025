a1f717d21210d552532d13d672bbc7ea2f91bb98
231B401 <231B401@juet.co.in>
2025-07-29 12:52:26 +0530

    Added solution to Lab Exercise 1

---
+++ LE1/sum_of_two_numbers.py
@@ -0,0 +1,11 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Take input from the user and convert it to integers
+... num1 = int(input("Enter first number: "))
+... num2 = int(input("Enter second number: "))
+... 
+... # Add the two numbers
+... sum = num1 + num2
+... 
+... # Print the result
+... print("The sum is:", sum)
